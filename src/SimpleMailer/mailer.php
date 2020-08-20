a21cc90053a9a20b56a8c4455796f0e83a5a9ca2
lostika <eugen.juhos@gmail.com>
2020-08-20 16:27:30 +0200

    allowed only xhr and post

--- src/SimpleMailer/mailer.php
+++ src/SimpleMailer/mailer.php
@@ -1,7 +1,6 @@
 <?php namespace JPackages\SimpleMailer;
 
 require_once JPACKAGE_VENDOR_PATH . '/autoload.php';
-//require_once __DIR__ . '/../../vendor/autoload.php';
 
 use JPackages\SimpleMailer\Cleaning\CleanRequest;
 use JPackages\SimpleMailer\Data\RequestData;
@@ -21,6 +20,15 @@ DotEnvBootstrap::make(JPACKAGE_START_PATH);
  */
 $requestData = new RequestData();
 /**
+ * Preparing response.
+ */
+$senderResponse = new SenderResponse(); // TODO make response json compatible
+if (!$requestData->getRequest()->ajax() && $requestData->getRequest()->isMethod('POST')) {
+	// only xhr request is allowed
+	return $senderResponse->setStatusCode(403)->send();
+}
+
+/**
  * Clean up the request, maybe contains some strange
  */
 $cleanRequest = new CleanRequest($requestData);
@@ -39,7 +47,7 @@ $responseContent->setFailedStatus();
 // ok, than check data validity
 if ($validator->success()){
 	// compose mailbody, and prepare e-mail for sending
-	$mailBody = new MailBody($requestData->getInput(),'hu');
+	$mailBody = new MailBody($requestData->getInput(),'sk');
 	$mailBase = new Base($mailBody);
 	// make the SMTP transport layer
 	$mailer = new MailTransport();
@@ -53,7 +61,7 @@ if ($validator->success()){
 // additionally push some marketing data, and make the error bag
 $responseContent->setErrors($validator->getMessageBag()->toArray())
 				->pushToMarketing(['submitted' => true]); // TODO make marketing blocks
-$senderResponse = new SenderResponse(); // TODO make response json compatible
+
 $senderResponse->setContent($responseContent);
 
 return $senderResponse->send();
